d3e5ced33c9fd037f3601862df41e8b0d14026e2
phucnt120822 <phucnt120822@gmail.com>
2026-03-20 16:55:01 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Get started</h1>
+        <Button type="primary">Hello Ant Design</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
